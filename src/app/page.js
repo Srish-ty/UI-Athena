a602e63d6e65489910685164c326e9a453ec10cf
Srishty M <srishtymangutte@gmail.com>
2024-09-14 18:25:50 +0530

    chore: ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,314 @@
 import Image from "next/image";
+import logo from "./images/logo1.png";
+import stats from "./images/stats.png";
+import brand0 from "./images/brand0.png";
+import brand2 from "./images/brand2.png";
+import brand3 from "./images/brand3.png";
+import brand4 from "./images/brand4.png";
+import brand5 from "./images/brand5.png";
+import brand6 from "./images/brand6.png";
+import use0 from "./images/use0.png";
+import use1 from "./images/use1.png";
+import use2 from "./images/use2.png";
+import glasscard0 from "./images/glasscard0.png";
+import glasscard1 from "./images/glasscard1.png";
+import centre from "./images/centre.png";
+import Footer from "./images/footer.png";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const AccordionDemo = () => {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+const RoundedButton = ({ children }) => (
+  <button className="rounded-3xl border-white border-2 px-5 py-3">
+    {children}
+  </button>
+);
+const ColorButton = ({ children }) => (
+  <button className="rounded-3xl border-white border-[1px] px-5 py-3 bg-gradient-to-r from-[#9021D4] to-[#26126F] text-white  hover:opacity-80 transition-opacity">
+    {children}
+  </button>
+);
+
+const NavBar = () => (
+  <nav className="flex justify-between items-center p-8">
+    <div className="flex items-center space-x-4">
+      <Image src={logo} alt="Logo" width={200} height={100} />
+    </div>
+    <div className="flex space-x-4">
+      <RoundedButton>Log in</RoundedButton>
+      <ColorButton>Create new account</ColorButton>
+    </div>
+  </nav>
+);
+
+const Title = ({ children }) => (
+  <div className="p-2 m-2">
+    <h1 className="text-5xl font-bold text-center">{children}</h1>
+  </div>
+);
+const SuperTitle = ({ children }) => (
+  <div className="py-2 px-24 mx-8 m-2">
+    <h1 className="text-5xl text-center font-extrabold">{children}</h1>
+  </div>
+);
+
+const SubTitle = ({ children }) => (
+  <div className="p-2 m-2">
+    <h2 className="text-xl text-center">{children}</h2>
+  </div>
+);
+
+const EmailField = () => {
+  return (
+    <div className="rounded-lg  my-4 p-6 max-w-3xl mx-auto">
+      <div className="space-y-4 max-w-lg mx-auto">
+        <div className="flex items-center rounded-full border-2 border-gray-500 border-primary h-[60px]">
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            className="flex-1 bg-transparent px-4 py-2 text-primary-foreground placeholder-muted-foreground focus:outline-none rounded-l-full"
+          />
+          <button
+            type="button"
+            className="bg-gradient-to-r h-[100%] from-[#5F0A87] to-[#A4508B] text-white px-6 py-2 rounded-full hover:opacity-90 transition-opacity"
+          >
+            Get Early Access -&gt;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const StatsField = () => (
+  <div className="relative w-full w-max mx-auto">
+    <div className="absolute w-full inset-0 -z-10 rounded-lg bg-gradient-to-r from-[#F1A410] via-[#822B6F] at-26% via-[#B8BA4B] at-66% via-[#93F7F3] at-85% to-[#4A64C2] blur-[80px] opacity-60" />
+    <div className="relative overflow-hidden w-full">
+      <Image src={stats} alt="image" width={1000} height={1000} />
+    </div>
+  </div>
+);
+
+const Brand = ({ src }) => (
+  <div className="flex items-center justify-center w-[160px]">
+    <Image src={src} alt="Brand" width={200} height={100} />
+  </div>
+);
+
+const useCases = [
+  {
+    src: use0,
+    title: "For HRTech & HR Vendors",
+    desc: "Deliver Al-Powered HR APPS and Experiences",
+  },
+  {
+    src: use1,
+    title: "For Staffing & Consulting firms",
+    desc: "Increase your revenue, retention and operating margin",
+  },
+  {
+    src: use2,
+    title: "For Large employers & Public sector",
+    desc: "Power Talent Management with Data and Connectivity.",
+  },
+];
+
+const UseCase = ({ src, title, description }) => (
+  <div className="flex flex-col justify-center items-start w-[300px]">
+    <Image src={src} alt="Use Case" width={80} height={80} />
+    <h3 className="text-xl font-extrabold mt-4">{title}</h3>
+    <p className="mt-2 text-left">{description}</p>
+  </div>
+);
+
+const Uses = () => {
+  return (
+    <div className="flex flex-col justify-between items-center p-8 w-full my-8">
+      <div className="w-[50vw]">
+        <Title>Leverage Athena.ai across industries & use-cases</Title>
+      </div>
+      <div className="flex flex-row justify-evenly w-full my-10">
+        {useCases.map((useCase, index) => (
+          <UseCase
+            key={index}
+            src={useCase.src}
+            title={useCase.title}
+            description={useCase.desc}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const brandContent = [brand0, brand2, brand3, brand4, brand5, brand6];
+
+const BrandsContainer = () => (
+  <div className="flex flex-col justify-between items-center p-8 w-full my-8">
+    <Title>Brands that trust us</Title>
+    <div className="flex flex-row justify-evenly w-full my-4">
+      {brandContent.map((src, index) => (
+        <Brand key={index} src={src} />
+      ))}
+    </div>
+  </div>
+);
+
+export const Advert = () => (
+  <div className="p-8 my-4 mx-auto w-[50vw] flex flex-col items-center">
+    <Title>The ultimate Talent Data Intelligence</Title>
+    <p className="text-lg my-2">Make Data and AI your competitive advantage.</p>
+  </div>
+);
+
+const GlassyCard = ({ heading, descr, children }) => (
+  <div
+    className="relative w-full max-w-[45%] mx-4 rounded-lg border border-solid border-[#44417a] bg-opacity-0 "
+    style={{
+      backgroundImage:
+        "linear-gradient(to bottom, #9893E3 0%, #44417a 10%, #090a2b 30%)",
+    }}
+  >
+    <div className="px-20  pt-10">
+      <h1 className="text-xl font-bold text-white mb-4">{heading}</h1>
+      <p className="text-sm text-white text-opacity-70">{descr}</p>
+    </div>
+    <div className="w-full">{children}</div>
+  </div>
+);
+
+const cardsInfo = [
+  {
+    heading: "Bring your own data",
+    descr:
+      "18 months ago, Yogini and I started Peerlist as a side project to effectively showcase our proof of work, a problem we faced firsthand. We realized that LinkedIn, a prominent Professional Network, is not catering effectively to ",
+    img: glasscard0,
+  },
+  {
+    heading: "Easy to Use ",
+    descr:
+      "18 months ago, Yogini and I started Peerlist as a side project to effectively showcase our proof of work, a problem we faced firsthand. We realized that LinkedIn, a prominent Professional Network, is not catering effectively to ",
+
+    img: glasscard1,
+  },
+];
+
+const CardsSec = () => {
+  return (
+    <div className="w-[85%] mx-auto">
+      <div className="flex flex-row justify-evenly w-full my-10">
+        {cardsInfo.map((card, index) => (
+          <GlassyCard key={index} heading={card.heading} descr={card.descr}>
+            <Image
+              src={card.img}
+              alt="card"
+              width={index == 0 ? 600 : 450}
+              height={index == 1 ? 500 : 100}
+              className="mx-auto"
+            />
+          </GlassyCard>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const BigCentralCont = () => {
+  return (
+    <div className="w-[80%] flex flex-row justify-between items-center mx-auto rounded-lg border border-solid border-[#44417a] bg-opacity-0 py-10 px-12">
+      <div className="w-1/3">
+        <h1 className="text-2xl my-4 font-bold">Flexible and customizable</h1>
+        <p className="text-sm my-6 text-white text-opacity-70">
+          18 months ago, Yogini and I started Peerlist as a side project to
+          effectively showcase our proof of work, a problem we faced firsthand.
+          We realized that LinkedIn, a prominent Professional Network, is not
+          catering effectively to
+        </p>
+      </div>
+      <div>
+        <Image src={centre} alt="image" width={500} height={300} />
+      </div>
+    </div>
+  );
+};
+
+const AccordionCont = () => {
+  return (
+    <div className="w-[80%] my-6 p-6 mx-auto flex flex-col justify-center items-center">
+      <Title>Frequently Asked Question </Title>
+      <AccordionDemo />
+    </div>
+  );
+};
+
+const FooterCont = () => {
+  return (
+    <div className="p-6 mx-auto flex flex-col justify-center items-center">
+      <Image src={Footer} alt="image" width={1200} height={500} />
+    </div>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <NavBar />
+      <section className="hero py-4 my-2">
+        <SuperTitle>
+          Enabling Companies to Build AI-Powered HRTech Solutions
+        </SuperTitle>
+        <SubTitle>
+          Go from idea to product with one API integration and without a single
+          line of code.
+        </SubTitle>{" "}
+      </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <EmailField />
+
+      <StatsField />
+
+      <BrandsContainer />
+      <Uses />
+
+      <Advert />
+      <CardsSec />
+      <BigCentralCont />
+
+      <AccordionCont />
+
+      <section className="hero h-[500px]"> first section </section>
+      <FooterCont />
     </div>
   );
 }
